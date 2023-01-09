f8f8f60cd485cf0c459064595b8f0157ec840914
Emma Forslund <emfo2102@student.miun.se>
2023-01-09 01:14:29 +0100

    Lagt till routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue';
 import AddList from '../views/AddListView.vue';
 import ListView from '../views/ListView.vue'; 
 import EditView from '../views/EditView.vue'; 
+import LoginView from '../views/LoginView.vue'; 
+import CategoryView from '../views/CategoriesView.vue';
+import SingleCategoryView from '../views/SingleCategoryView.vue';
+import AddCategoryView from '../views/AddCategoryView.vue';
+import AddMember from '../views/AddMember.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,25 +15,95 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: ListView,
+      beforeEnter: (to, from, next) => {
+        if (localStorage.getItem("token")) {
+          next()
+        } else {
+          next('/login');
+        }
+      }
     },
     {
       path: "/addlist",
       name: "addlist",
       component: AddList,
+      beforeEnter: (to, from, next) => {
+        if (localStorage.getItem("token")) {
+          next()
+        } else {
+          next('/login');
+        }
+      }
       
-    },
-    {
-      path: "/list",
-      name: "list",
-      component: ListView,
-      
-    },{
+    }
+    ,{
       path: '/edit/:id',
       name: 'edit',
       component: EditView,
-      props: true
+      props: true,
+      beforeEnter: (to, from, next) => {
+        if (localStorage.getItem("token")) {
+          next()
+        } else {
+          next('/login');
+        }
+      }
+    },
+    {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    component: CategoryView,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem("token")) {
+        next()
+      } else {
+        next('/login');
+      }
     }
+  },
+  {
+    path: '/singlecategory/:id',
+    name: 'singlecategory',
+    component: SingleCategoryView,
+    props: true,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem("token")) {
+        next()
+      } else {
+        next('/login');
+      }
+    }
+  },
+  {
+    path: '/addcategory',
+    name: 'addcategory',
+    component: AddCategoryView,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem("token")) {
+        next()
+      } else {
+        next('/login');
+      }
+    }
+  },
+  {
+    path: '/addmember',
+    name: 'addmember',
+    component: AddMember,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem("token")) {
+        next()
+      } else {
+        next('/login');
+      }
+    }
+  },
   ]
 })
 
